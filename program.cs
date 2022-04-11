b7f6db61ba223615bbb1812072cff789b7cea2d4
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2022-04-12 02:00:58 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace lab5try1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger x = new BigInteger("15");
+            //Console.WriteLine(x);
+
+            Console.WriteLine(BigInteger.a);
+
+        }
+    }
+}
