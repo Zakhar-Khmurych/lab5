c098fd54ea6cce76811b4246debf77e4bfac75d5
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2022-04-12 15:07:15 +0300

    3.0

--- Biginteger.cs
+++ Biginteger.cs
@@ -1,5 +1,12 @@
- public class BigInteger
-    {   
+using System;
+using System.Linq;
+
+namespace lab5try1
+{
+    public class BigInteger
+    {
+        private bool _is_negative = false;
+        
         private int[] _numbers;
 
         private string _numbersString;
@@ -16,8 +23,14 @@
                 i += 1;
             }
         }
-   public override string ToString()
+
+        public override string ToString()
         {
+            if (_is_negative)
+            {
+                _numbersString += "-";
+            }
+            
             for (int i = 0; i < _numbers.Length; i++)
             {
                 _numbersString += Convert.ToString(_numbers[i]);
@@ -25,7 +38,7 @@
 
             return _numbersString;
         }
-  public BigInteger Add(BigInteger another)
+        public BigInteger Add(BigInteger another)
         {
 
             int maxLength = this._numbers.Length;
@@ -93,4 +106,95 @@
             BigInteger result = new BigInteger(s);
             return result;
         }
- }
+        
+
+        public BigInteger Sub(BigInteger another)
+        {
+            bool isNegative=false;
+            int maxLength = this._numbers.Length;
+            int minLength = another._numbers.Length;
+
+            if (this._numbers.Length < another._numbers.Length)
+            {
+                maxLength = another._numbers.Length;
+                minLength = this._numbers.Length;
+            }
+
+            int[] num1Copy;
+            int[] num2Copy;
+
+            num1Copy = new int[maxLength];
+            num2Copy = new int[maxLength];
+
+
+            for (int i = this._numbers.Length-1; i >=0; i--)
+            {
+                num1Copy[i + maxLength - this._numbers.Length] = this._numbers[i];
+            }
+
+            for (int i = another._numbers.Length - 1; i>=0; i--)
+            {
+                num2Copy[i + maxLength - another._numbers.Length] = another._numbers[i];
+            }
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (this._numbers[i] < another._numbers[i])
+                {
+                    isNegative = true;
+                    break;
+                }
+            }
+
+            int[] diff;
+            diff = new int[maxLength+1];
+            
+            if (isNegative)
+            {
+                for (int i = maxLength-1; i >=0; i--)
+                {
+                    diff[i+1] = num2Copy[i] - num1Copy[i];
+                }
+            }
+            else
+            {
+                for (int i = maxLength-1; i >=0; i--)
+                {
+                    diff[i+1] = num1Copy[i] - num2Copy[i];
+                }
+            }
+            
+
+            for (int i = diff.Length - 1; i > 0; i--)
+            {
+                if (diff[i] < 0)
+                {
+                    diff[i] = diff[i] + 10;
+                    diff[i - 1] = diff[i - 1] - 1;
+                }
+            }
+           
+            
+            string d ="";
+            
+            if ((diff[0] == 0) && (diff.Length>1))
+            {
+                for (int i = 1; i < diff.Length; i++)
+                {
+                    d += diff[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < diff.Length; i++)
+                {
+                    d += diff[i];
+                }
+            }
+            
+            BigInteger result = new BigInteger(d);
+            result._is_negative = isNegative;
+            return result;
+        }
+    }
+}
